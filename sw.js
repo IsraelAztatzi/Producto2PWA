e2c413fb0fc96682f6a3ca3dc820f5abf366e066
IsraelAztatzi <84100702+IsraelAztatzi@users.noreply.github.com>
2022-11-30 00:33:32 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+var nombreCache = 'uno';
+self.addEventListener(
+    'install',
+    function(event){
+        event.waitUntil(
+            caches.open(nombreCache)
+            .then(
+                function(cache){
+                    cache.addAll(
+                        [
+                            './script.js',
+                            'w3.css',
+                            'index.html',
+                            'manifest.json',
+                            './icon.png'
+                        ]
+                    );
+                }
+            )
+        );
+    }
+);
+
+self.addEventListener(
+    'fetch', 
+    function(event) {
+        event.respondWith( 
+            caches.match(event.request)
+            .then(
+                function(respuesta){
+                    if(respuesta){ 
+                        console.log('La respuesta proviene del cache.');
+                        return respuesta; 
+                    }
+                    else{
+                        console.log('La respuesta no proviene del cache.');
+                        return fetch(event.request);
+                    }
+                }
+            ) 
+        );
+    }
+); 
